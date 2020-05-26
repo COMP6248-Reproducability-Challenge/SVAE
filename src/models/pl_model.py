fixed loss function and img scaling (except rot)

src/models/pl_model.py
@@ -44,34 +44,44 @@ class Model(SpatialVAE):
     Loading the desired dataset as prompted by the user
     """
     if self.dataset == 'MNIST':
-      transform = torchvision.transforms.Compose([torchvision.transforms.ToTensor()])
-      mnist_train = torchvision.datasets.MNIST('../../src/data/mnist/',
-                                                train=True,
-                                                download=True,
-                                                transform=transform).data.float()
+      transform = torchvision.transforms.Compose(
+          [torchvision.transforms.ToTensor()])
+      mnist_train = torchvision.datasets.MNIST(
+          '../../src/data/mnist/',
+          train=True,
+          download=True,
+          transform=transform).data.float()
       mnist_val = torchvision.datasets.MNIST('../../src/data/mnist/',
-                                              train=False,
-                                              download=True,
-                                              transform=transform).data.float()
-      self.train_dataset = mnist_train
-      self.val_dataset = mnist_val
+                                             train=False,
+                                             download=True,
+                                             transform=transform).data.float()
+      self.train_dataset = mnist_train / 255.0
+      self.val_dataset = mnist_val / 255.0
     elif self.dataset == 'MNIST_Rotated':
-      mnist_train = torch.Tensor(np.load('../../src/data/mnist_rotated/images_train.npy'))
-      mnist_val = torch.Tensor(np.load('../../src/data/mnist_rotated/images_test.npy'))
-      self.train_dataset = mnist_train
-      self.val_dataset = mnist_val
+      mnist_train = torch.Tensor(
+          np.load('../../src/data/mnist_rotated/images_train.npy'))
+      mnist_val = torch.Tensor(
+          np.load('../../src/data/mnist_rotated/images_test.npy'))
+      self.train_dataset = mnist_train / 255.0
+      self.val_dataset = mnist_val / 255.0
     elif self.dataset == 'MNIST_Translated':
-      mnist_train = torch.Tensor(np.load('../../src/data/mnist_rotated_translated/images_train.npy'))
-      mnist_val = torch.Tensor(np.load('../../src/data/mnist_rotated_translated/images_test.npy'))
-      self.train_dataset = mnist_train
-      self.val_dataset = mnist_val
+      mnist_train = torch.Tensor(
+          np.load('../../src/data/mnist_rotated_translated/images_train.npy'))
+      mnist_val = torch.Tensor(
+          np.load('../../src/data/mnist_rotated_translated/images_test.npy'))
+      self.train_dataset = mnist_train / 255.0
+      self.val_dataset = mnist_val / 255.0
     elif self.dataset == 'Galaxy_Zoo':
-      mnist_train = torch.Tensor(np.load('../../src/data/galaxy_zoo/galaxy_zoo_train.npy'))
-      mnist_val = torch.Tensor(np.load('../../src/data/galaxy_zoo/galaxy_zoo_test.npy'))
-      self.train_dataset = mnist_train
-      self.val_dataset = mnist_val
+      mnist_train = torch.Tensor(
+          np.load('../../src/data/galaxy_zoo/galaxy_zoo_train.npy'))
+      mnist_val = torch.Tensor(
+          np.load('../../src/data/galaxy_zoo/galaxy_zoo_test.npy'))
+      self.train_dataset = mnist_train / 255.0
+      self.val_dataset = mnist_val / 255.0
     else:
-      print("Please choose between the available datasets: MNIST, MNIST_Rotated, MNIST_Translated and Galaxy_Zoo")
+      print(
+          "Please choose between the available datasets: MNIST, MNIST_Rotated, MNIST_Translated and Galaxy_Zoo"
+      )
 
   def configure_optimizers(self):
     return Adam(self.parameters(), lr=1e-3)
@@ -99,10 +109,7 @@ class Model(SpatialVAE):
     loss_train = self.loss(x, reconstruction, mu, logstd, self.pi)
     # adding logging
     tqdm_dict = {'train_loss': loss_train}
-    output = OrderedDict({
-      'loss': loss_train,
-      'progress_bar': tqdm_dict
-    })
+    output = OrderedDict({'loss': loss_train, 'progress_bar': tqdm_dict})
     return output
 
   def validation_step(self, batch, batch_idx):
@@ -120,10 +127,7 @@ class Model(SpatialVAE):
     reconstruction, mu, logstd = self.forward(x)
     loss_val = self.loss(x, reconstruction, mu, logstd, self.pi)
     tqdm_dict = {'val_loss': loss_val}
-    output = OrderedDict({
-      'val_loss': loss_val,
-      'progress_bar': tqdm_dict
-    })
+    output = OrderedDict({'val_loss': loss_val, 'progress_bar': tqdm_dict})
     return output
 
   def training_epoch_end(self, outputs):
@@ -135,10 +139,7 @@ class Model(SpatialVAE):
       train_loss_mean += train_loss
 
     train_loss_mean /= len(outputs)
-    result = {
-      'end_train_loss': train_loss_mean,
-      'step': self.epoch
-    }
+    result = {'end_train_loss': train_loss_mean, 'step': self.epoch}
     self.logs['Training Loss'] = train_loss_mean.item()
     return {'log': result}
 
@@ -151,10 +152,7 @@ class Model(SpatialVAE):
       val_loss_mean += val_loss
 
     val_loss_mean /= len(outputs)
-    result = {
-      'end_val_loss': val_loss_mean,
-      'step': self.epoch
-    }
+    result = {'end_val_loss': val_loss_mean, 'step': self.epoch}
     self.logs['Validation Loss'] = val_loss_mean.item()
     return {'log': result}
 

src/models/svae.py
@@ -19,7 +19,7 @@ def make_normalized_grid(width, height):
     Shape [width, height, 3].
   """
   x = np.linspace(-1, 1, width)
-  y = np.linspace(-1, 1, height)
+  y = np.linspace(1, -1, height)
   xv, yv = np.meshgrid(x, y)
   ones = np.ones((width, height))  # Helper dim. Makes linear transform easier.
   grid = np.stack((xv, yv, ones), axis=2)
@@ -260,47 +260,49 @@ class SpatialVAE(pl.LightningModule):
     """
     kl_divergence = torch.tensor(0.0, dtype=torch.float32)
     # Compare the two images, no logits as already present in model
-    size = x.size(1) * 3
-    print(size)
-    reconstruction_loss = -F.binary_cross_entropy(reconstruction, x) * size
+    size = x.size(1) * x.size(3) * x.size(2)
+    reconstruction_loss = F.binary_cross_entropy(reconstruction, x) * size
     # temp remove before uncommenting rotation and translation
-    logstd = logstd[:, 1:]
-    mu = mu[:, 1:]
-    logstd = logstd[:, 2:]
-    mu = mu[:, 2:]
+    # logstd = logstd[:, 1:]
+    # mu = mu[:, 1:]
+    # logstd = logstd[:, 2:]
+    # mu = mu[:, 2:]
 
     # Custom equation defined in Spatial VAE (Bepler et al) (2019)
     # Calculate KL Divergence for rotation variable
-    # -0.5 - logstd + log_sigma + var/(2*sigma^2)
-    # if self.has_rotation:
-    #   theta_std = logstd[:, :1]
-    #   logstd = logstd[:, 1:]
-    #   mu = mu[:, 1:]
-    #   theta_var = 2 * theta_std  #  using log power rule log(x^2) == 2*log(x)
-    #   kl_d_rotation = torch.sum(-0.5 - theta_std + torch.log(sigma) +
-    #                             (theta_var).exp() / (2 * sigma.pow(2)))
-    #   kl_divergence += kl_d_rotation
-    #   print(kl_d_rotation)
+    # pseudo -0.5 - logstd + log_sigma + var/(2*sigma^2)
+    if self.has_rotation:
+      theta_std = logstd[:, :1]
+      logstd = logstd[:, 1:]
+      mu = mu[:, 1:]
+      theta_var = 2 * theta_std  #  using log power rule log(x^2) == 2*log(x)
+      kl_d_rotation = torch.sum(-0.5 - theta_std + torch.log(sigma) +
+                                (theta_var).exp() / (2 * sigma.pow(2)))
+      kl_divergence += kl_d_rotation
+      print("ROT", kl_d_rotation)
     # Implementation based of Kingma and Welling (2014)
     # calculate KL Divergence for translation variables
-    # if self.has_translation:
-    #   t_std = logstd[:, :2]
-    #   logstd = logstd[:, 2:]
-    #   t_mu = mu[:, :2]
-    #   mu = mu[:, 2:]
-    #   t_var = 2 * t_std
-    #   kl_d_translation = -0.5 * torch.sum(1 + (t_var) - t_mu.pow(2) -
-    #                                       (t_var).exp())
-    #   kl_divergence += kl_d_translation
-
-    # compare the KL Divergence for the unconstrained latent variables (MINE)
-    log_var = 2 * logstd
-    kl_d_unconstrained = -0.5 * torch.mean(1 + (log_var) - mu.pow(2) -
-                                           (log_var).exp())
+    if self.has_translation:
+      t_std = logstd[:, :2]
+      logstd = logstd[:, 2:]
+      t_mu = mu[:, :2]
+      mu = mu[:, 2:]
+      # t_var = 2 * t_std
+      # kl_d_translation = -0.5 * torch.mean(1 + (t_var) - t_mu.pow(2) -
+      #                                      (t_var).exp())
+      kl_d_translation = -t_std + 0.5 * torch.exp(
+          t_std)**2 + 0.5 * t_mu**2 - 0.5
+      kl_d_translation = torch.mean(torch.sum(kl_d_translation, 1))
+      kl_divergence += kl_d_translation
+      print("TRANSL", kl_d_translation)
+    # compare the KL Divergence for the unconstrained latent variables (OURS)
+    # log_var = 2 * logstd
+    # kl_d_unconstrained = -0.5 * torch.mean(1 + (log_var) - mu.pow(2) -
+    #                                        (log_var).exp())
     # (THEIRS)
-    # kl_d_unconstrained = -logstd + 0.5 * torch.exp(
-    #     logstd)**2 + 0.5 * mu**2 - 0.5
-    # kl_d_unconstrained = torch.mean(torch.sum(kl_d_unconstrained, 1))
+    kl_d_unconstrained = -logstd + 0.5 * torch.exp(
+        logstd)**2 + 0.5 * mu**2 - 0.5
+    kl_d_unconstrained = torch.mean(torch.sum(kl_d_unconstrained, 1))
     kl_divergence += kl_d_unconstrained
-    print("-------------->", kl_d_unconstrained)
-    return reconstruction_loss - kl_divergence
+    elbo = reconstruction_loss - kl_divergence
+    return elbo